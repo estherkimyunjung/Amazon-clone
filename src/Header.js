f7a4847a3ed43495170f4bd368f4decf61a09f14
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-10 22:01:45 -0600

    clean unuse file and basic component setup

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>HEADER HERE</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDhvLqMyrAVvemw9SJ5HKtrS1Rk67olofg",
+  authDomain: "ecommerce-app-e9e30.firebaseapp.com",
+  projectId: "ecommerce-app-e9e30",
+  storageBucket: "ecommerce-app-e9e30.appspot.com",
+  messagingSenderId: "862736347430",
+  appId: "1:862736347430:web:9edeb8d82d1adc6d2191e7",
+  measurementId: "G-Q2G5JP3728",
+};
