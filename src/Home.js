3628c9f701aff43b48565b351bb07547d1e0913e
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-10 23:24:05 -0600

    add banner image

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Home.css";
 
 function Home() {
   return (
     <div className="home">
-      <h1>HOME HERE</h1>
+      <div className="home_container">
+        <img
+          className="home_image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220)_.jpg"
+          alt="prime video poster"
+          srcset=""
+        />
+      </div>
     </div>
   );
 }
