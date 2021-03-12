168a706658a025e7bfb645ed5ec402e6a7a24bc1
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-12 17:30:36 -0600

    use redux for che kout items

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,17 +1,22 @@
 import { SportsBasketballTwoTone } from "@material-ui/icons";
 import React from "react";
 import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer";
+import { useStateValue } from "./StateProvider";
+
 import "./Subtotal.css";
 
 function Subtotal() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="subtotal">
       <CurrencyFormat
         renderText={(value) => (
           <>
             <p>
-              Subtotal (0 items):
-              <strong>0</strong>
+              Subtotal ({basket.length} items):
+              <strong>{value}</strong>
             </p>
             <small className="subtotal_gift">
               <input type="checkbox" /> This order contains a gift
@@ -19,7 +24,7 @@ function Subtotal() {
           </>
         )}
         decimalScale={2}
-        value={0}
+        value={getBasketTotal(basket)}
         displayType={"text"}
         thousandSeparator={true}
         prefix={"$"}
