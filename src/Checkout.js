5367c4f810671966d1fa8cd220fdc35e464cba05
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-13 02:54:44 -0600

    deployed

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import "./Checkout.css";
 import { useStateValue } from "./StateProvider";
 
 function Checkout() {
-  const [{ basket }, dispatch] = useStateValue();
+  const [{ basket, user }, dispatch] = useStateValue();
 
   return (
     <div className="checkout">
@@ -17,6 +17,7 @@ function Checkout() {
           srcset=""
         />
         <div>
+          <h3>Hello, {user?.email}</h3>
           <h2 className="checkout_title">Your Shopping Basket</h2>
           {basket.map((item) => (
             <CheckoutProduct
