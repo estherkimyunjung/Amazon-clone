40e5237b0a07671f0704e840ba8495542171537c
estherkimyunjung <estherkimyunjung@gmail.com>
2021-03-15 16:06:43 -0500

    deployed

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5001/ecommerce-app-e9e30/us-central1/api", //THE API (cloud function) URL
+  baseURL: "https://us-central1-ecommerce-app-e9e30.cloudfunctions.net/api", //THE API (cloud function) URL
+  // "http://localhost:5001/ecommerce-app-e9e30/us-central1/api"
 });
 
 export default instance;
